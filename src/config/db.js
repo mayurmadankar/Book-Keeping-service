2d9f69db6298d566b184070a98c1290d7c176715
Mayur <madankarmayur5@gmail.com>
2025-04-24 22:04:32 +0530

    mongo_url

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const baseUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/";
-console.log(baseUrl);
+const baseUrl = process.env.MONGODB_URL;
+// console.log(baseUrl);
 
 export const connectDB = async () => {
   try {
